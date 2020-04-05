f09856e53a0aa2f21fb53cf0466bab247773038b
Jackson Ellenburg <jdellenburg6591@gmail.com>
2020-04-05 14:05:46 -0400

    Create AlertMovie.java

---
+++ AlertMovie.java
@@ -0,0 +1,17 @@
+public class AlertMovie extends Movie {
+    private Movie[] movies;
+    private boolean isShowing;
+    private String showtimes;
+    
+    public boolean isShowing() {
+        
+    }
+    
+    public String getShowtimes() {
+        
+    }
+    
+    public void updateMovie(String showtimes) {
+        
+    }
+}
